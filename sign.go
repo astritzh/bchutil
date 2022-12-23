6ade09850b6b64357fe02f0c5c68a92513c601c1
Astrit Zhushi <astrit@luno.com>
2022-12-23 11:50:33 +0100

    bchutil: update btcutil

--- sign.go
+++ sign.go
@@ -5,12 +5,14 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"github.com/btcsuite/btcd/btcec"
+
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcec/v2/ecdsa"
+	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
 )
 
 const (
@@ -23,11 +25,13 @@ const (
 func RawTxInSignature(tx *wire.MsgTx, idx int, subScript []byte,
 	hashType txscript.SigHashType, key *btcec.PrivateKey, amt int64) ([]byte, error) {
 
-	hash := calcBip143SignatureHash(subScript, txscript.NewTxSigHashes(tx), hashType, tx, idx, amt)
-	signature, err := key.Sign(hash)
-	if err != nil {
-		return nil, fmt.Errorf("cannot sign tx input: %s", err)
-	}
+	// It return a nil byte array whenever a prevOutput is requested.
+	// Taproot checking will then be skipped.
+	// It is only reasonable if RawTxInSignature() doesn't support taproot and we are not supporting it now.
+	// from https://github.com/wcsiu/bchutil/blob/master/sign.go
+	var canned = txscript.NewCannedPrevOutputFetcher(nil, 0)
+	hash := calcBip143SignatureHash(subScript, txscript.NewTxSigHashes(tx, canned), hashType, tx, idx, amt)
+	signature := ecdsa.Sign(key, hash)
 
 	return append(signature.Serialize(), byte(hashType|SigHashForkID)), nil
 }
@@ -104,7 +108,7 @@ func calcBip143SignatureHash(subScript []byte, sigHashes *txscript.TxSigHashes,
 	// If anyone can pay isn't active, then we can use the cached
 	// hashPrevOuts, otherwise we just write zeroes for the prev outs.
 	if hashType&txscript.SigHashAnyOneCanPay == 0 {
-		sigHash.Write(sigHashes.HashPrevOuts[:])
+		sigHash.Write(sigHashes.HashPrevOutsV0[:])
 	} else {
 		sigHash.Write(zeroHash[:])
 	}
@@ -115,7 +119,7 @@ func calcBip143SignatureHash(subScript []byte, sigHashes *txscript.TxSigHashes,
 	if hashType&txscript.SigHashAnyOneCanPay == 0 &&
 		hashType&sigHashMask != txscript.SigHashSingle &&
 		hashType&sigHashMask != txscript.SigHashNone {
-		sigHash.Write(sigHashes.HashSequence[:])
+		sigHash.Write(sigHashes.HashSequenceV0[:])
 	} else {
 		sigHash.Write(zeroHash[:])
 	}
@@ -146,7 +150,7 @@ func calcBip143SignatureHash(subScript []byte, sigHashes *txscript.TxSigHashes,
 	// pre-image.
 	if hashType&sigHashMask != txscript.SigHashSingle &&
 		hashType&sigHashMask != txscript.SigHashNone {
-		sigHash.Write(sigHashes.HashOutputs[:])
+		sigHash.Write(sigHashes.HashOutputsV0[:])
 	} else if hashType&sigHashMask == txscript.SigHashSingle && idx < len(tx.TxOut) {
 		var b bytes.Buffer
 		wire.WriteTxOut(&b, 0, 0, tx.TxOut[idx])
@@ -248,7 +252,7 @@ func SignatureScript(tx *wire.MsgTx, idx int, subscript []byte, hashType txscrip
 		return nil, err
 	}
 
-	pk := (*btcec.PublicKey)(&privKey.PublicKey)
+	pk := (*btcec.PublicKey)(privKey.PubKey())
 	var pkData []byte
 	if compress {
 		pkData = pk.SerializeCompressed()
